379f2b837ea999a6b487beb0b6f1964ddbae8562
Kevin Ortiz <63128557+keorgtz@users.noreply.github.com>
2026-06-23 16:52:28 -0600

    Changes Finance Initials

--- src/AegiFinance.Web/obj/Debug/net9.0/AegiFinance.Web.AssemblyInfo.cs
+++ src/AegiFinance.Web/obj/Debug/net9.0/AegiFinance.Web.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AegiFinance.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a619ed9581dd32bb248c4514c69b051558829fd4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2ff2bd556a2b4914a6fe825ca8c6fe2d6b9b95d3")]
 [assembly: System.Reflection.AssemblyProductAttribute("AegiFinance.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AegiFinance.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
